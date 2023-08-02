0b9690fdf26bcf88f4f004e435cbff16ff204a8c
seunghwanYun <asdryzx2@gmail.com>
2023-08-02 22:56:56 +0900

    feat: Category add button method 구현 및 Datasource 구현

--- Udemy/Todoey-iOS13/Todoey/Controllers/CategoryViewController.swift
+++ Udemy/Todoey-iOS13/Todoey/Controllers/CategoryViewController.swift
@@ -4,7 +4,7 @@ import CoreData
 
 class CategoryViewController: UITableViewController {
     
-    var CategoryArray: [Category] = []
+    var categoryArray: [Category] = []
     
     let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
 
@@ -15,19 +15,43 @@ class CategoryViewController: UITableViewController {
     // MARK: - Add New Categories
     
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        let alert = UIAlertController(title: "Add New Category", message: "", preferredStyle: .alert)
         
+        let action = UIAlertAction(title: NSLocalizedString("Add Category", comment: "Default acition"), style: .default) { action in
+            guard let userTextField = alert.textFields?.first?.text else { return }
+            
+            let category = Category(context: self.context)
+            category.name = userTextField
+            
+            if !userTextField.isEmpty {
+                self.categoryArray.append(category)
+                self.saveItems()
+                
+                let indexPathOfNewItem = IndexPath(row: self.itemArray.count - 1, section: 0)
+                self.tableView.insertRows(at: [indexPathOfNewItem], with: .none)
+            }
+            
+        }
+        
+        
+        alert.addTextField() { alertTextField in
+            alertTextField.placeholder = "Create new category"
+        }
+        
+        alert.addAction(action)
+        present(alert, animated: true)
     }
     
 
     // MARK: - TableView Datasource Methods
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return CategoryArray.count
+        return categoryArray.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell", for: indexPath)
-        let category = CategoryArray[indexPath.row]
+        let category = categoryArray[indexPath.row]
         cell.textLabel?.text = category.name
 
         return cell
