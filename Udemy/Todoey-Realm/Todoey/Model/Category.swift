a63f5573ffdc8cf4b4d2346e08c45f9d661e1db6
seunghwanYun <asdryzx2@gmail.com>
2023-08-03 13:36:55 +0900

    feat: Realm package 추가, Category, Item class 파일 생성

---
+++ Udemy/Todoey-Realm/Todoey/Model/Category.swift
@@ -0,0 +1,9 @@
+//
+//  Category.swift
+//  Todoey
+//
+//  Created by yun on 2023/08/03.
+//  Copyright © 2023 App Brewery. All rights reserved.
+//
+
+import Foundation
---
+++ Udemy/Todoey-Realm/Todoey/Model/Item.swift
@@ -0,0 +1,15 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by yun on 2023/08/03.
+//  Copyright © 2023 App Brewery. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class Item: Object {
+    @Persisted var title: String
+    @Persisted var done: Bool = false
+}
