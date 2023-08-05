2deec89186de4c0b613b0c4d596025c9f2d75ccf
seunghwanYun <asdryzx2@gmail.com>
2023-08-05 15:08:05 +0900

    Unit Test project 추가

---
+++ UnitTest/PhotoApp/PhotoApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PhotoApp
+//
+//  Created by yun on 2023/08/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
