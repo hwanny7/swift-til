02f8d3c1f6d2bb27ab45af4d3fa8cfc3bfc8539f
seunghwanYun <asdryzx2@gmail.com>
2023-08-10 15:15:57 +0900

    add: spotify ui 실습

--- AutoLayout/autolayout-course-master/DesignChallenges/1-Anchors/BasicAnchors/BasicAnchors/Challenge.swift
+++ AutoLayout/autolayout-course-master/DesignChallenges/1-Anchors/BasicAnchors/BasicAnchors/Challenge.swift
@@ -25,15 +25,14 @@ class Challenge: UIViewController {
 
     func setupViews() {
 
-        // Comment in incrementally ...
-        //        let offlineLabel = makeLabel(withText: "Offline")
-        //        let offlineSwitch = makeSwitch(isOne: false)
-        //        let offlineSublabel = makeSubLabel(withText: "When you go offline, you'll only be able to play the music and podcasts you've downloaded.")
+        let offlineLabel = makeLabel(withText: "Offline")
+        let offlineSwitch = makeSwitch(isOn: false)
+                let offlineSublabel = makeSubLabel(withText: "When you go offline, you'll only be able to play the music and podcasts you've downloaded.")
         //
-        //        let crossfadeLabel = makeBoldLabel(withText: "Crossfade")
-        //        let crossfadeMinLabel = makeSubLabel(withText: "0s")
-        //        let crossfadeMaxLabel = makeSubLabel(withText: "12s")
-        //        let crossfadeProgressView = makeProgressView()
+                let crossfadeLabel = makeBoldLabel(withText: "Crossfade")
+                let crossfadeMinLabel = makeSubLabel(withText: "0s")
+                let crossfadeMaxLabel = makeSubLabel(withText: "12s")
+                let crossfadeProgressView = makeProgressView()
         //
         //        let gaplessPlaybackLabel = makeLabel(withText: "Gapless Playback")
         //        let gaplessPlaybackSwitch = makeSwitch(isOne: true)
@@ -43,26 +42,45 @@ class Challenge: UIViewController {
         //
         //        let enableNormalizationLabel = makeLabel(withText: "Enable Audio Normalization")
         //        let enableNormalizationSwitch = makeSwitch(isOne: true)
-        //
-        //        view.addSubview(offlineLabel)
-        //        view.addSubview(offlineSwitch)
-        //        view.addSubview(offlineSublabel)
-        //
-        //        view.addSubview(crossfadeLabel)
-        //        view.addSubview(crossfadeMinLabel)
-        //        view.addSubview(crossfadeProgressView)
-        //        view.addSubview(crossfadeMaxLabel)
-        //
-        //        view.addSubview(gaplessPlaybackLabel)
-        //        view.addSubview(gaplessPlaybackSwitch)
-        //
-        //        view.addSubview(hideSongsLabel)
-        //        view.addSubview(hideSongsSwitch)
-        //
-        //        view.addSubview(enableNormalizationLabel)
-        //        view.addSubview(enableNormalizationSwitch)
 
-        // Start your layout here...
+
+        view.addSubview(offlineLabel)
+        view.addSubview(offlineSwitch)
+        view.addSubview(offlineSublabel)
+        view.addSubview(crossfadeLabel)
+        view.addSubview(crossfadeMinLabel)
+        view.addSubview(crossfadeProgressView)
+        view.addSubview(crossfadeMaxLabel)
+        
+        
+        NSLayoutConstraint.activate([
+            offlineLabel.leadingAnchor.constraint(equalTo: view.layoutMarginsGuide.leadingAnchor),
+            offlineLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: margin),
+            
+            offlineSwitch.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor),
+            offlineSwitch.topAnchor.constraint(equalTo: view.topAnchor, constant: margin),
+            
+            offlineSublabel.topAnchor.constraint(equalTo: offlineSwitch.bottomAnchor, constant: margin),
+            offlineSublabel.leadingAnchor.constraint(equalTo: view.layoutMarginsGuide.leadingAnchor),
+            offlineSublabel.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor),
+            
+            
+            crossfadeLabel.topAnchor.constraint(equalTo: offlineSublabel.bottomAnchor, constant: spacing),
+            crossfadeLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            
+            crossfadeMinLabel.topAnchor.constraint(equalTo: crossfadeLabel.bottomAnchor, constant: spacing),
+            crossfadeMinLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: margin),
+            
+            crossfadeProgressView.centerYAnchor.constraint(equalTo: crossfadeMinLabel.centerYAnchor),
+            crossfadeProgressView.leadingAnchor.constraint(equalTo: crossfadeMinLabel.trailingAnchor, constant: 4),
+            crossfadeProgressView.trailingAnchor.constraint(equalTo: crossfadeMaxLabel.leadingAnchor, constant: -4),
+            
+            crossfadeMaxLabel.centerYAnchor.constraint(equalTo: crossfadeMinLabel.centerYAnchor),
+            crossfadeMaxLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -margin),
+            
+            
+        ])
+        
 
     }
 }
--- AutoLayout/autolayout-course-master/DesignChallenges/2-IntrinsicContentSize/SpotifyPlayer/SpotifyPlayer/Challenge.swift
+++ AutoLayout/autolayout-course-master/DesignChallenges/2-IntrinsicContentSize/SpotifyPlayer/SpotifyPlayer/Challenge.swift
@@ -18,29 +18,59 @@ class Challenge: UIViewController {
     func setupViews() {
 
         // Comment in incrementally...
-        //        let albumImage = makeImageView(named: "rush")
-        //        let trackLabel = makeTrackLabel(withText: "Tom Sawyer")
-        //        let albumLabel = makeAlbumLabel(withText: "Rush • Moving Pictures (2011 Remaster)")
+                let albumImage = makeImageView(named: "rush")
+                let trackLabel = makeTrackLabel(withText: "Tom Sawyer")
+                let albumLabel = makeAlbumLabel(withText: "Rush • Moving Pictures (2011 Remaster)")
         //
-        //        let playButton = makePlayButton()
-        //        let previewStartLabel = makePreviewLabel(withText: "0:00")
-        //        let previewEndLabel = makePreviewLabel(withText: "0:30")
-        //        let progressView = makeProgressView()
+                let playButton = makePlayButton()
+                let previewStartLabel = makePreviewLabel(withText: "0:00")
+                let previewEndLabel = makePreviewLabel(withText: "0:30")
+                let progressView = makeProgressView()
         //
         //        let spotifyButton = makeSpotifyButton(withText: "PLAY ON SPOTIFY")
         //
-        //        view.addSubview(albumImage)
-        //        view.addSubview(trackLabel)
-        //        view.addSubview(albumLabel)
+                view.addSubview(albumImage)
+                view.addSubview(trackLabel)
+                view.addSubview(albumLabel)
         //
-        //        view.addSubview(playButton)
-        //        view.addSubview(previewStartLabel)
-        //        view.addSubview(progressView)
-        //        view.addSubview(previewEndLabel)
+                view.addSubview(playButton)
+                view.addSubview(previewStartLabel)
+                view.addSubview(progressView)
+                view.addSubview(previewEndLabel)
         //
         //        view.addSubview(spotifyButton)
 
         // Start your layout here...
+        
+        NSLayoutConstraint.activate([
+            albumImage.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            albumImage.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            albumImage.heightAnchor.constraint(equalTo: albumImage.widthAnchor, multiplier: 1),
+            albumImage.widthAnchor.constraint(equalToConstant: view.bounds.width),
+            
+            trackLabel.topAnchor.constraint(equalTo: albumImage.bottomAnchor, constant: 16),
+            trackLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            
+            albumLabel.topAnchor.constraint(equalTo: trackLabel.bottomAnchor, constant: 8),
+            albumLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            albumLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+        
+            
+            playButton.topAnchor.constraint(equalTo: albumLabel.bottomAnchor, constant: 8),
+            playButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 8),
+            playButton.heightAnchor.constraint(equalToConstant: buttonHeight),
+            playButton.widthAnchor.constraint(equalToConstant: buttonHeight),
+            
+            previewStartLabel.centerYAnchor.constraint(equalTo: playButton.centerYAnchor),
+            previewStartLabel.leadingAnchor.constraint(equalTo: playButton.trailingAnchor, constant: 4),
+            
+            previewEndLabel.centerYAnchor.constraint(equalTo: playButton.centerYAnchor),
+            previewEndLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            
+            progressView.centerYAnchor.constraint(equalTo: playButton.centerYAnchor),
+            progressView.leadingAnchor.constraint(equalTo: previewStartLabel.trailingAnchor, constant: 4),
+            progressView.trailingAnchor.constraint(equalTo: previewEndLabel.leadingAnchor, constant: -4),
+        ])
 
 
     }
