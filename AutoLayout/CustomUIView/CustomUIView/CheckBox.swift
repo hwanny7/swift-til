0d997cc63a153626337b9f80e4ed6742183b92ed
seunghwanYun <asdryzx2@gmail.com>
2023-08-11 08:31:34 +0900

    ADD: Custom Class

---
+++ AutoLayout/CustomUIView/CustomUIView/CheckBox.swift
@@ -0,0 +1,52 @@
+
+import UIKit
+
+@IBDesignable
+class Checkbox: UIControl {
+    private weak var imageView: UIImageView!
+    
+    private var image: UIImage {
+        return checked ? UIImage(systemName: "checkmark.square.fill")! : UIImage(systemName: "square")!
+    }
+    
+    @IBInspectable
+    public var checked: Bool = false {
+        didSet {
+            imageView.image = image
+        }
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setup()
+    }
+    
+    private func setup() {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFit
+        imageView.image = UIImage(systemName: "checkmark.square.fill")
+        self.imageView = imageView
+        addSubview(imageView)
+        
+        NSLayoutConstraint.activate([
+            imageView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            imageView.bottomAnchor.constraint(equalTo: bottomAnchor),
+            imageView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            imageView.topAnchor.constraint(equalTo: topAnchor),
+        ])
+        
+        addTarget(self, action: #selector(touchUpinside), for: .touchUpInside)
+    }
+    
+    @objc func touchUpinside() {
+        checked = !checked
+        sendActions(for: .valueChanged)
+    }
+    
+}
